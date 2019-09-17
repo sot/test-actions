dea86256d55b99f6bb2925627346798291396012
James Kristoff <james.kristoff@ipa.harvard.edu>
2019-09-17 16:02:51 -0400

    fixed some lint issues.

--- sample.py
+++ sample.py
@@ -1,12 +1,12 @@
-import sys
-#  blah 
-x  = (1,2,3)
+# blah 
+x = (1,2,3)
+
 
 def func(y):
-     print(z)
-     print(2)
+    print(z)
+    print(2)
 
-def function_to_close():
+
+def function_was_too_close():
     print('looking for lint warnings.')
-
 
