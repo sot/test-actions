f4325626c495c2d54951faca0e0ad494de337872
Javier Gonzalez <javierggt@yahoo.com>
2025-10-31 11:24:19 -0400

    mv agasc -> test_pkg

---
+++ test_pkg/test_pkg/setup.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from setuptools import setup
+
+setup(
+    name="test_pkg",
+    version="0.0.0",
+    description="Test package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="http://example.com/test_pkg",
+    packages=[
+        "test_pkg",
+    ],
+)
---
+++ test_pkg/test_pkg/test_pkg/__init__.py
@@ -0,0 +1,1 @@
+from .test_pkg import *  # noqa
